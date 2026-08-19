22e9493a5fe9f54b3fceb3c17e9f0e59aaecea6b
Nidhi Kumari <259925890+nidhikumari0020@users.noreply.github.com.>
2026-08-19 17:39:51 +0530

    learn useState and useState advance

---
+++ 09-usestate-advance/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+
+const [num, setNum] = useState(0)
+
+const btnClicked = ()=>{
+  setNum(20)
+  console.log(num)
+
+}
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={btnClicked}>Click</button>
+      
+    </div>
+  )
+}
+
+
+export default App
